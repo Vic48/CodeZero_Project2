a7c3497dcb047648f3f4d09c863e79094e04a1ff
lianqi541 <holianqi@gmail.com>
2023-03-09 13:22:26 +0800

    Bullethole+Scrump

--- Assets/Scripts/WeaponFire.cs
+++ Assets/Scripts/WeaponFire.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class WeaponFire : MonoBehaviour
 {
-    public float cooldown = 0.3f;
+    public float cooldown = 0.5f;
 
     private bool isFiring = false;
+
+    [SerializeField] private GameObject _bulletHolePrefab; //Bullet hole
+    [SerializeField] private GameObject _scrumpPrefab; //Scrump
 
     // Start is called before the first frame update
     void Start()
@@ -34,13 +37,42 @@ public class WeaponFire : MonoBehaviour
         RaycastHit hit;
         if (this.GetComponentInParent<Mouse>().GetShootHitPos(out hit))
         {
+            //returns true if we click the mouse button
+            RaycastHit hitInfo; //Contains raycast hit infomation
+            if (Physics.Raycast(origin: transform.position, direction: transform.forward, out hitInfo))
+            {
+                float x = Random.Range(0,2);
+                
+                if (x == 0)
+                {
+                    //returns true if ray touches something
+                    GameObject obj = Instantiate(_bulletHolePrefab, hitInfo.point + (hitInfo.normal * 0.01f), Quaternion.identity);
+                    //Instantiating the bullet hole object
+                    obj.transform.LookAt(hitInfo.point, hitInfo.normal);
+
+                    //can show bullet hole image if hit wall/ground etc - can fade away after awhile
+                    Destroy(obj, 1);
+                }
+
+                else if (x == 1)
+                {
+                    //returns true if ray touches something
+                    GameObject obj = Instantiate(_scrumpPrefab, hitInfo.point + (hitInfo.normal * 0.01f), Quaternion.identity);
+                    //Instantiating the bullet hole object
+                    obj.transform.LookAt(hitInfo.point, hitInfo.normal);
+
+                    //can show bullet hole image if hit wall/ground etc - can fade away after awhile
+                    Destroy(obj, 1);
+                    Debug.Log("scrump");
+                }
+
+            }
+
             //check if item can be hit
             if (hit.collider.GetComponent<TargetScript>() != null)
             {
                 //if target can be hit, destroy
                 hit.collider.GetComponent<TargetScript>().DoHit();
-
-                //can show bullet hole image if hit wall/ground etc - can fade away after awhile
             }
         }
 
